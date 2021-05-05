25af9ca8952f96705aec0347fbb2e0e74636c13a
Kuttyajai <83656758+Kuttyajai@users.noreply.github.com>
2021-05-05 11:57:18 +0530

    Delete clock.py

--- clock.py
+++
@@ -1,9 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-from love import send_love
-
-sched = BlockingScheduler()
-
-# Schedule job_function to be called every two hours
-sched.add_job(send_love, 'interval', seconds=5)
-
-sched.start()
